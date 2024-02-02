ec1a4f5aa42a428a76cbbfb13f19cf950691cac2
ViktorKapustianyk <vityakap@gmail.com>
2024-02-02 11:31:06 +0200

    Configure Hibernate-mapping and added the configuration file. Now the connection to the database is opened through the configuration file

--- src/main/java/org/example/service/MySessionFactory.java
+++ src/main/java/org/example/service/MySessionFactory.java
@@ -3,26 +3,11 @@ package org.example.service;
 import org.example.entity.*;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
-
-import java.util.Properties;
 
 public class MySessionFactory {
     private static MySessionFactory instance;
     private final SessionFactory sessionFactory;
     private MySessionFactory() {
-        Properties properties = new Properties();
-        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
-        properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-        properties.put(Environment.URL, "jdbc:mysql://localhost:3306/movie");
-        properties.put(Environment.USER, "root");
-        properties.put(Environment.PASS, "root");
-        properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-        properties.put(Environment.HBM2DDL_AUTO, "validate");
-//
-//        properties.put("hibernate.connection.useUnicode", true);
-//        properties.put("hibernate.connection.characterEncoding", "UTF-8");
-//        properties.put("hibernate.connection.charSet", "UTF-8");
 
         sessionFactory = new Configuration()
                 .addAnnotatedClass(EntityActor.class)
@@ -39,7 +24,7 @@ public class MySessionFactory {
                 .addAnnotatedClass(EntityRental.class)
                 .addAnnotatedClass(EntityStaff.class)
                 .addAnnotatedClass(EntityStore.class)
-                .setProperties(properties)
+                .configure("hibernate_mysql.cfg.xml")
                 .buildSessionFactory();
     }
 
